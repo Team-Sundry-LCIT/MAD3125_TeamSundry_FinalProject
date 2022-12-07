48388737ebd7dd98306d453db197923c8702b636
0871063 <c0871063@mylambton.ca>
2022-12-07 18:11:41 -0500

    Load the fragment in MainActivity

--- app/src/main/java/mad3125/teamsundry/finalproject/MainActivity.java
+++ app/src/main/java/mad3125/teamsundry/finalproject/MainActivity.java
@@ -1,8 +1,12 @@
 package mad3125.teamsundry.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+<<<<<<< HEAD
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+=======
+import androidx.fragment.app.FragmentManager;
+>>>>>>> a9de33c (Load the fragment in MainActivity)
 
 import android.os.Bundle;
 
@@ -16,10 +20,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+<<<<<<< HEAD
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         controller = Navigation.findNavController(this,R.id.fragmentContainer);
 
+=======
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().add(R.id.employee_list_container,new EmployeeListFragment(),"EmployeeList").commit();
+>>>>>>> a9de33c (Load the fragment in MainActivity)
     }
 }
