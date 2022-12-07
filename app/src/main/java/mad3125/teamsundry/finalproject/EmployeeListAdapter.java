becd0a588a57352d13d05b415ed5609a51af65be
0871063 <c0871063@mylambton.ca>
2022-12-07 18:26:41 -0500

    Employee List Adapter Class

---
+++ app/src/main/java/mad3125/teamsundry/finalproject/EmployeeListAdapter.java
@@ -0,0 +1,35 @@
+package mad3125.teamsundry.finalproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import mad3125.teamsundry.finalproject.Part1.Employee;
+
+public class EmployeeListAdapter extends ArrayAdapter {
+    private Context context;
+    int resLayout;
+
+    public EmployeeListAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+        this.context = context;
+        this.resLayout = resource;
+    }
+
+    @Override
+    public int getCount() {
+        return Employee.employeeList.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        
+    }
+}
