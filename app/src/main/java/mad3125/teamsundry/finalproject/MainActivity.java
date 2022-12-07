dc076430be4f13cd030132716aee443ba421ae22
Tilak <c0868747@mylambton.ca>
2022-12-06 19:22:49 -0500

    initial commit

---
+++ app/src/main/java/mad3125/teamsundry/finalproject/MainActivity.java
@@ -0,0 +1,14 @@
+package mad3125.teamsundry.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
