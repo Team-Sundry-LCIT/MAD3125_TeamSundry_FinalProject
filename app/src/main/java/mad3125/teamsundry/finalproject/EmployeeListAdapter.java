3f66a21b0cf3e60af667ad1f68591f4301ed25ed
0871063 <c0871063@mylambton.ca>
2022-12-07 19:38:53 -0500

    Employee List Adapter - Set data to view , Floating button click, Search style

--- app/src/main/java/mad3125/teamsundry/finalproject/EmployeeListAdapter.java
+++ app/src/main/java/mad3125/teamsundry/finalproject/EmployeeListAdapter.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import mad3125.teamsundry.finalproject.Part1.Employee;
+import mad3125.teamsundry.finalproject.databinding.EmployeeRowLayoutBinding;
 
 public class EmployeeListAdapter extends ArrayAdapter {
     private Context context;
@@ -30,6 +31,11 @@ public class EmployeeListAdapter extends ArrayAdapter {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        EmployeeRowLayoutBinding binding = EmployeeRowLayoutBinding.inflate(LayoutInflater.from(context));
 
+        binding.nameTV.setText(Employee.employeeList.get(position).getName());
+        binding.typeTV.setText(Employee.employeeList.get(position).getClass().getName());
+        binding.incomeTV.setText(Double.toString(Employee.employeeList.get(position).getAnnualIncome()));
+        return binding.getRoot();
     }
 }
--- app/src/main/java/mad3125/teamsundry/finalproject/EmployeeListFragment.java
+++ app/src/main/java/mad3125/teamsundry/finalproject/EmployeeListFragment.java
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
+import mad3125.teamsundry.finalproject.databinding.ActivityMainBinding;
 import mad3125.teamsundry.finalproject.databinding.FragmentEmployeeListBinding;
 
 public class EmployeeListFragment extends Fragment {
@@ -11,7 +12,6 @@ public class EmployeeListFragment extends Fragment {
 =======
 
     private FragmentEmployeeListBinding binding;
-    ExtendedFloatingActionButton addEmployee;
 
     public EmployeeListFragment() {
     }
@@ -19,15 +19,23 @@ public class EmployeeListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_employee_list,container,false);
+        binding = FragmentEmployeeListBinding.inflate(getLayoutInflater(),container,false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.addEmployee.shrink();
+        binding.addEmployee.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
 
-        binding.addEmployee.setOnClickListener(v -> {
-        });
+                        binding.addEmployee.extend();
+                    }
+                });
+
     }
 >>>>>>> 160dedd (Add Binding for employee list fragment)
 }
