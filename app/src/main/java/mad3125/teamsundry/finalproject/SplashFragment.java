bc0ad5588e7c3a5c6412d9230e5bc1d89f493779
Tilak <c0868747@mylambton.ca>
2022-12-07 20:51:34 -0500

    -add custom splash screen

---
+++ app/src/main/java/mad3125/teamsundry/finalproject/SplashFragment.java
@@ -0,0 +1,44 @@
+package mad3125.teamsundry.finalproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import mad3125.teamsundry.finalproject.databinding.FragmentSplashBinding;
+
+public class SplashFragment extends Fragment {
+
+    FragmentSplashBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentSplashBinding.inflate(getLayoutInflater(),container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Animation animation = AnimationUtils.loadAnimation(requireContext(),R.anim.anim_zoom_in_logo);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Navigation.findNavController(requireActivity(),R.id.fragmentContainer).navigate(R.id.action_splashFragment_to_employeeListFragment);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+        binding.logo.startAnimation(animation);
+    }
+}
