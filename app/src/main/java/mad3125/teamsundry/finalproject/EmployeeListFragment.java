9a38bb5edfdcccaf3dfe95668bfaf60812eb6d8c
0871063 <c0871063@mylambton.ca>
2022-12-08 13:53:20 -0500

    Search method

--- app/src/main/java/mad3125/teamsundry/finalproject/EmployeeListFragment.java
+++ app/src/main/java/mad3125/teamsundry/finalproject/EmployeeListFragment.java
@@ -3,16 +3,24 @@ package mad3125.teamsundry.finalproject;
 <<<<<<< HEAD
 =======
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+<<<<<<< HEAD
 >>>>>>> aecb178 (List click event)
+=======
+import androidx.appcompat.widget.SearchView;
+>>>>>>> 87f6bf2 (Search method)
 import androidx.fragment.app.Fragment;
 
+import mad3125.teamsundry.finalproject.Part1.Employee;
 import mad3125.teamsundry.finalproject.databinding.FragmentEmployeeListBinding;
 
 public class EmployeeListFragment extends Fragment {
@@ -20,6 +28,7 @@ public class EmployeeListFragment extends Fragment {
 =======
 
     private FragmentEmployeeListBinding binding;
+    EmployeeListAdapter adapter;
 
     public EmployeeListFragment() {
     }
@@ -34,6 +43,10 @@ public class EmployeeListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        adapter = new EmployeeListAdapter(requireContext(), R.layout.employee_row_layout);
+        binding.employeeList.setAdapter(adapter);
+
         binding.addEmployee.shrink();
         binding.addEmployee.setOnClickListener(
                 new View.OnClickListener() {
@@ -51,7 +64,31 @@ public class EmployeeListFragment extends Fragment {
             }
         });
 
-    }
+        binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // Override onQueryTextSubmit method which is call when submit query is searched
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // If the list contains the search query than filter the adapter
+                // using the filter method with the query as its argument
+                if (Employee.employeeList.contains(query)) {
+                    adapter.getFilter().filter(query);
+                } else {
+                    // Search query not found in List View
+                    Toast.makeText(requireContext(), "Not found", Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+
+            // This method is overridden to filter the adapter according
+            // to a search query when the user is typing search
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        }
 
     private void loadEmployee(int position){
     }
