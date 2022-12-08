98cc252a56d9bdeefcecddcd94d99ff3fc935667
Tilak <c0868747@mylambton.ca>
2022-12-07 20:49:28 -0500

    -remove android default splash screen -add buildFeatures and dependencies

--- app/src/main/java/mad3125/teamsundry/finalproject/MainActivity.java
+++ app/src/main/java/mad3125/teamsundry/finalproject/MainActivity.java
@@ -1,14 +1,25 @@
 package mad3125.teamsundry.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
+import mad3125.teamsundry.finalproject.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    NavController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        controller = Navigation.findNavController(this,R.id.fragmentContainer);
+
     }
 }
