d4a612f3fb8fe9df76ef5456fa4c02ac91693a82
0871063 <c0871063@mylambton.ca>
2022-12-07 20:31:00 -0500

    List click event

--- app/src/main/java/mad3125/teamsundry/finalproject/EmployeeListFragment.java
+++ app/src/main/java/mad3125/teamsundry/finalproject/EmployeeListFragment.java
@@ -1,10 +1,18 @@
 package mad3125.teamsundry.finalproject;
 
+<<<<<<< HEAD
+=======
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+>>>>>>> aecb178 (List click event)
 import androidx.fragment.app.Fragment;
 
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
-
-import mad3125.teamsundry.finalproject.databinding.ActivityMainBinding;
 import mad3125.teamsundry.finalproject.databinding.FragmentEmployeeListBinding;
 
 public class EmployeeListFragment extends Fragment {
@@ -31,11 +39,24 @@ public class EmployeeListFragment extends Fragment {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-
+                        addEmployee();
                         binding.addEmployee.extend();
                     }
                 });
 
+        binding.employeeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                loadEmployee(position);
+            }
+        });
+
+    }
+
+    private void loadEmployee(int position){
+    }
+
+    private void addEmployee(){
     }
 >>>>>>> 160dedd (Add Binding for employee list fragment)
 }
