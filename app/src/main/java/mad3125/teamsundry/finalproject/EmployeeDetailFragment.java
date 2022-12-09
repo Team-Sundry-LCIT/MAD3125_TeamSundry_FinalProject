456c40d108983d35c91d70569170bec84f7d2620
Alice <c0874203@mylambton.ca>
2022-12-08 20:29:27 -0500

    show data.

--- app/src/main/java/mad3125/teamsundry/finalproject/EmployeeDetailFragment.java
+++ app/src/main/java/mad3125/teamsundry/finalproject/EmployeeDetailFragment.java
@@ -9,16 +9,26 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import mad3125.teamsundry.finalproject.Part1.Employee;
 import mad3125.teamsundry.finalproject.databinding.FragmentEmployeeDetailBinding;
 
 public class EmployeeDetailFragment extends Fragment {
 
     private FragmentEmployeeDetailBinding binding;
+    private Employee employee;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentEmployeeDetailBinding.inflate(getLayoutInflater(), container, false);
+
+
+        employee = (Employee) getArguments().getSerializable("data");
+
+        binding.textViewName.setText(employee.getName());
+        binding.textViewTitle.setText(employee.getClass().getSimpleName());
+        binding.textViewDetail.setText(employee.toString());
+
         return binding.getRoot();
     }
 }
