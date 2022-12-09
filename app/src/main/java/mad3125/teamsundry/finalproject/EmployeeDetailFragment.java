e17343a9242082a02d82bba6139a81e55b01ffac
Alice <c0874203@mylambton.ca>
2022-12-08 20:13:01 -0500

    implement some design.

--- app/src/main/java/mad3125/teamsundry/finalproject/EmployeeDetailFragment.java
+++ app/src/main/java/mad3125/teamsundry/finalproject/EmployeeDetailFragment.java
@@ -1,6 +1,24 @@
 package mad3125.teamsundry.finalproject;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import mad3125.teamsundry.finalproject.databinding.FragmentEmployeeDetailBinding;
+
 public class EmployeeDetailFragment extends Fragment {
+
+    private FragmentEmployeeDetailBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentEmployeeDetailBinding.inflate(getLayoutInflater(), container, false);
+        return binding.getRoot();
+    }
 }
